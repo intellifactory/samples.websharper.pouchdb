b16feb27383f53d6838ccd400045849475de1eb5
Gansperger István <gansperger@gmail.com>
2014-09-09 18:22:50 +0200

    build

--- build.cmd
+++ build.cmd
@@ -1,5 +1,5 @@
 @echo off
 setlocal
 set PATH=%PATH%;%ProgramFiles(x86)%\MSBuild\12.0\Bin
-::powershell tools/restore-packages.ps1
+powershell tools/restore-packages.ps1
 MSBuild.exe /p:Configuration=Release /verbosity:minimal
