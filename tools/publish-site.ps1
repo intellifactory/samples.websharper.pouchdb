357c8aaac74d52b297591fa3be9061f8d48b9fa6
Sandor Rakonczai <lunnia@inf.elte.hu>
2015-04-03 11:44:37 +0200

    Emptied branch

--- tools/publish-site.ps1
+++
@@ -1,9 +0,0 @@
-# creates build/html
-rm -r build -errorAction ignore
-$d = mkdir build
-$d = mkdir build/html
-cp -r WebSharper.Samples.PouchDB/Content build/html/
-cp -r WebSharper.Samples.PouchDB/*.css build/html/
-cp -r WebSharper.Samples.PouchDB/*.html build/html/
-cp -r WebSharper.Samples.PouchDB/*.png build/html/
-cp -r WebSharper.Samples.PouchDB/google-code-prettify build/html/
